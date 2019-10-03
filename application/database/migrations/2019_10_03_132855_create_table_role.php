4310e99893b3204cc912ad358f8eacf663cce5c3
warkop619 <navigator619@gmail.com>
2019-10-03 13:37:52 +0700

    tabel role dan seedernya

---
+++ application/database/migrations/2019_10_03_132855_create_table_role.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->increments('role_id');
+            $table->string('role_name')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role');
+    }
+}
---
+++ application/database/seeds/RoleTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('role')->insert([
+            'role_name'     => 'Administrator',
+            'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
+            'end_date'      => date('Y-m-d', strtotime("31-12-2040")),
+            'created_at'    => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('role')->insert([
+            'role_name'     => 'Departemen',
+            'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
+            'end_date'      => date('Y-m-d', strtotime("31-12-2040")),
+            'created_at'    => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('role')->insert([
+            'role_name'     => 'Checker',
+            'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
+            'end_date'      => date('Y-m-d', strtotime("31-12-2040")),
+            'created_at'    => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('role')->insert([
+            'role_name'     => 'Loket',
+            'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
+            'end_date'      => date('Y-m-d', strtotime("31-12-2040")),
+            'created_at'    => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
