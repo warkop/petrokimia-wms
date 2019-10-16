70b4ec55579a5ba56dcd5fa1fec3924c0e0dcc97
warkop619 <navigator619@gmail.com>
2019-10-16 13:50:13 +0700

    tambah kolom

---
+++ application/database/migrations/2019_10_16_134913_alter_table_material_trans.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableMaterialTrans extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('material_trans', function (Blueprint $table) {
+            $table->integer('id_realisasi_material')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('material_trans', function (Blueprint $table) {
+            //
+        });
+    }
+}
