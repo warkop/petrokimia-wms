f782f4a508fc7f9e119fe11a97b6a0abc1da5836
Mohammad Khafid Barokum <navigator619@gmail.com>
2019-10-18 01:28:16 +0700

    update scope query

--- application/app/Scopes/EndDateScope.php
+++ application/app/Scopes/EndDateScope.php
@@ -17,6 +17,6 @@ class EndDateScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
-        $builder->where('end_date', null);
+        $builder->where('end_date', null)->orWhere('end_date', '>', date('Y-m-d'));
     }
 }
