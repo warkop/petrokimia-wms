c29024f10df8caf977c088b3bcb4b59d8228a534
Mohammad Khafid Barokum <navigator619@gmail.com>
2019-10-18 01:03:41 +0700

    install and configuring sentry for production

---
+++ application/app/Http/Middleware/SentryUserContext.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Sentry\State\Scope;
+
+class SentryUserContext
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && app()->bound('sentry')) {
+            \Sentry\configureScope(function (Scope $scope): void {
+                $scope->setUser([
+                    'id'    => Auth::user()->id,
+                    'email' => Auth::user()->email,
+                    'name'  => Auth::user()->name,
+                    'username'  => Auth::user()->username,
+                ]);
+            });
+        }
+
+        return $next($request);
+    }
+}
