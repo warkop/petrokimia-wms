872ebcf3065cc3eb6362c644fd52cf1dc7188aad
warkop619 <navigator619@gmail.com>
2019-10-24 15:09:42 +0700

    api starter

---
+++ application/app/Http/Controllers/API/AktivitasController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Models\Aktivitas;
+use App\Http\Resources\AktivitasResource;
+use Illuminate\Http\Response;
+
+class AktivitasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new AktivitasResource(Aktivitas::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $aktivitas = Aktivitas::find($id);
+        if (!empty($aktivitas)) {
+            return (new AktivitasResource($aktivitas))->response()->setStatusCode(200);
+        } else {
+            return response()->json([
+                'message' => 'Resource not found',
+                'status_code' => Response::HTTP_NOT_FOUND
+            ], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- application/routes/api.php
+++ application/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Models\Aktivitas;
+use App\Http\Resources\AktivitasResource;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +18,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('aktivitas', function () {
+//     return (new AktivitasResource(Aktivitas::paginate()))->response()->setStatusCode(200);
+// });
+Route::get('aktivitas', 'API\AktivitasController@index');
+
+// Route::get('aktivitas/{id}', function ($id) {
+//     $obj = Aktivitas::find($id);
+//     if (!empty($obj)) {
+//         return (new AktivitasResource(Aktivitas::find($id)))->response()->setStatusCode(200);
+//     } else {
+//         return (new AktivitasResource(new Aktivitas))->response()->setStatusCode(400);
+//     }
+// });
+Route::get('aktivitas/{aktivitas}', 'API\AktivitasController@show');
