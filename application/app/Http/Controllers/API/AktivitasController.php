6c506963b7547a10b471daa752bea01d35306684
warkop619 <navigator619@gmail.com>
2019-10-31 16:26:08 +0700

    aktivitas api

--- application/app/Http/Controllers/API/AktivitasController.php
+++ application/app/Http/Controllers/API/AktivitasController.php
@@ -25,8 +25,6 @@ class AktivitasController extends Controller
             'code' => Response::HTTP_OK],
         ], Response::HTTP_OK);
 
-        // dump(Aktivitas::search($search)->get());
-
         return $obj;
     }
 
@@ -117,35 +115,16 @@ class AktivitasController extends Controller
         }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
