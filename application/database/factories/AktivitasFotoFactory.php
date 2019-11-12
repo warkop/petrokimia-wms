1c8e4bdd02ea0f3bc16cca58072e7f304d66ccbf
warkop619 <navigator619@gmail.com>
2019-11-12 10:08:43 +0700

    ganti 755

--- application/database/factories/AktivitasFotoFactory.php
+++ application/database/factories/AktivitasFotoFactory.php
@@ -7,13 +7,13 @@ $factory->define(AktivitasFoto::class, function (Faker $faker) {
     $id_aktivitas_harian = $faker->numberBetween(1, 50);
     $dir = storage_path('app\\public') . '\\aktivitas_harian\\' . $id_aktivitas_harian;
     if (!file_exists(storage_path('app\\public') . '\\aktivitas_harian\\')) {
-        mkdir(storage_path('app\\public') . '\\aktivitas_harian\\', 777);
+        mkdir(storage_path('app\\public') . '\\aktivitas_harian\\', 755);
         if (!file_exists($dir)) {
-            mkdir($dir, 777);
+            mkdir($dir, 755);
         }
     } else {
         if (!file_exists($dir)) {
-            mkdir($dir, 777);
+            mkdir($dir, 755);
         }
     }
 
