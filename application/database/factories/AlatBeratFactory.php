d2d1d8ee6a9cd79fb922b6c6494b40bea748faa6
warkop619 <navigator619@gmail.com>
2019-11-13 18:39:39 +0700

    alat berat factory

--- application/database/factories/AlatBeratFactory.php
+++ application/database/factories/AlatBeratFactory.php
@@ -5,9 +5,10 @@ use Faker\Generator as Faker;
 
 $factory->define(AlatBerat::class, function (Faker $faker) {
     return [
-        'id_kategori' => $faker->numberBetween(1, 3),
+        'id_kategori'   => $faker->numberBetween(1, 3),
         'nomor_lambung' => $faker->regexify('[A-Z]+[0-9]+[A-Z]{2,4}'),
-        'created_by' => 1,
-        'created_at' => now(),
+        'status'        => $faker->numberBetween(0, 1),
+        'created_by'    => 1,
+        'created_at'    => now(),
     ];
 });
