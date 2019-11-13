d18b3cae70be3af1b7c747f4758598974813e4ff
warkop619 <navigator619@gmail.com>
2019-11-13 09:46:06 +0700

    yo

--- application/database/factories/AktivitasFotoFactory.php
+++ application/database/factories/AktivitasFotoFactory.php
@@ -21,21 +21,15 @@ $factory->define(AktivitasFoto::class, function (Faker $faker) {
     $height = 480;
 
     $file_ori = $faker->image($dir, $width, $height, 'cats', false);
-    $ext = explode('.', $file_ori);
-
-    // $enc = md5($ext[0]) . '.' . $ext[1];
-
-    // rename($dir . '\\' . $file_ori, $dir . '\\' . $enc);
 
     $size = filesize($dir . '/' . $file_ori);
-    // $size = '';
 
     return [
         'id_aktivitas_harian'   => $id_aktivitas_harian,
         'id_foto_jenis'         => $faker->numberBetween(1, 4),
         'foto'                  => $file_ori,
         'size'                  => $size,
-        // 'lat'                   => $faker->latitude(-7, -6),
-        // 'lng'                   => $faker->longitude(112, 117),
+        'lat'                   => $faker->latitude(-7, -6),
+        'lng'                   => $faker->longitude(112, 113),
     ];
 });
