ff2107e5b960fbfce9be9e2830bcef2b71bb0379
warkop619 <navigator619@gmail.com>
2019-11-23 10:35:47 +0700

    migrasi tambah tabel master keluhan dan keluhan operator

---
+++ application/database/migrations/2019_11_23_100946_create_table_keluhan_operator.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableKeluhanOperator extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keluhan_operator', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('keterangan');
+            $table->integer('id_operator');
+            $table->integer('id_keluhan');
+            $table->integer('created_by');
+            $table->datetime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keluhan_operator');
+    }
+}
