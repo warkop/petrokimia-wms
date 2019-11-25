eb36d196693ed077161d7370ea3bc091c679ab82
warkop619 <navigator619@gmail.com>
2019-11-25 14:23:14 +0700

    delete sentry

--- application/config/app.php
+++ application/config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        // Sentry\Laravel\ServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -225,7 +224,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        // 'Sentry' => Sentry\Laravel\Facade::class,
     ],
 
 ];
