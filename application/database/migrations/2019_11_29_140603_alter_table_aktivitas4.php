e3ab9a0ddd1343fb15f732be83f822e84fcb9eca
warkop619 <navigator619@gmail.com>
2019-11-29 14:09:01 +0700

    migrasi tabel aktivitas

---
+++ application/database/migrations/2019_11_29_140603_alter_table_aktivitas4.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableAktivitas4 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('aktivitas', function (Blueprint $table) {
+            $table->integer('anggaran_tkbm')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('aktivitas', function (Blueprint $table) {
+            $table->dropColumn('anggaran_tkbm');
+        });
+    }
+}
