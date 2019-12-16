212661f1984776eaaf4ef8eaead1ec6b1b07d1eb
warkop619 <navigator619@gmail.com>
2019-12-16 09:54:26 +0700

    layout

--- application/app/Http/Resources/LayoutAreaResource.php
+++ application/app/Http/Resources/LayoutAreaResource.php
@@ -14,12 +14,21 @@ class LayoutAreaResource extends Resource
      */
     public function toArray($request)
     {
+        // if ($this->sum($this->areaStok->jumlah) == $this->kapasitas) {
+        //     $warna = 'red';
+        // } else if ($this->sum($this->areaStok->jumlah) == $this->kapasitas* 70/100) {
+        //     $warna = 'yellow';
+        // } else {
+        //     $warna = 'green';
+        // }
+
         return [
             'id'        => $this->id,
             'id_gudang' => $this->id_gudang,
             'nama'      => $this->nama,
             'kapasitas' => $this->kapasitas,
             'tipe'      => $this->tipe,
+            // 'warna'     => $warna,
             'koordinat' => json_decode($this->koordinat),
         ];
     }
