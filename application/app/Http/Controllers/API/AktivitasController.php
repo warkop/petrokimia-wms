207aa6685765f9596ad1b74664c4e3eb3b6f5e8f
warkop619 <navigator619@gmail.com>
2019-12-23 09:39:41 +0700

    notif

--- application/app/Http/Controllers/API/AktivitasController.php
+++ application/app/Http/Controllers/API/AktivitasController.php
@@ -77,11 +77,12 @@ class AktivitasController extends Controller
             ->first();
 
         $rencanaTkbm = RencanaTkbm::where('id_rencana', $rencanaHarian->id)
-            ->where(function ($query) {
-                $query->where('end_date');
-                $query->orWhere('end_date', '>', now());
-            })
-            ->get();
+        ->join('rencana_harian', 'rencana_tkbm.id_rencana', '=', 'rencana_harian.id')
+        ->where(function ($query) {
+            $query->where('end_date');
+            $query->orWhere('end_date', '>=', now());
+        })
+        ->get();
 
         foreach ($rencanaTkbm as $key) {
             $user = Users::where('id_tkbm', $key->id_tkbm)->first();
