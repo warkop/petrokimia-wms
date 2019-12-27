fe3b91ab02b4347e22d44c9a6e342c0a4835433d
warkop619 <navigator619@gmail.com>
2019-12-27 12:42:02 +0700

    migrasi

---
+++ application/database/migrations/2019_12_27_093058_alter_table_aktivitas_harian3.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableAktivitasHarian3 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('aktivitas_harian', function (Blueprint $table) {
+            $table->integer('draft')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('aktivitas_harian', function (Blueprint $table) {
+            $table->dropColumn('draft');
+        });
+    }
+}
---
+++ application/database/migrations/2019_12_27_093207_alter_table_realisasi3.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableRealisasi3 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('realisasi', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('realisasi', function (Blueprint $table) {
+            //
+        });
+    }
+}
