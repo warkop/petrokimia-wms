ae850c045257700a3860ac1c4d99ca5398e2a2cb
warkop619 <navigator619@gmail.com>
2019-12-29 10:05:27 +0700

    tambah kolom id_gudang

---
+++ application/database/migrations/2019_12_29_095338_alter_table_material_adjustment2.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableMaterialAdjustment2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('material_adjustment', function (Blueprint $table) {
+            $table->integer('id_gudang')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('material_adjustment', function (Blueprint $table) {
+            $table->dropColumn('id_gudang');
+        });
+    }
+}
