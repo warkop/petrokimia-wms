f2a0d0dcfd6e5a8e333139ad81b28908e2e208a8
warkop619 <navigator619@gmail.com>
2020-01-10 19:12:07 +0700

    handler remove json decode

--- application/app/Exceptions/Handler.php
+++ application/app/Exceptions/Handler.php
@@ -39,7 +39,7 @@ class Handler extends ExceptionHandler
         
         $jsonResponse->setData(array_merge($original, [
             'code'          => $exception->status,
-            'errors'        => json_decode(Handler::expandDotNotationKeys((array) $original['errors']), true),
+            'errors'        => Handler::expandDotNotationKeys((array) $original['errors']),
         ]));
 
         return $jsonResponse;
@@ -51,7 +51,7 @@ class Handler extends ExceptionHandler
         foreach ($array as $key => $value) {
             array_set($result, $key, $value);
         }
-        
+        dd($result);
         return $result;
     }
 
