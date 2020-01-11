a3f74b551f6e4a358b3371d639c80ee57c848438
warkop619 <navigator619@gmail.com>
2020-01-11 17:50:00 +0700

    yo

--- application/app/Http/Controllers/API/AktivitasController.php
+++ application/app/Http/Controllers/API/AktivitasController.php
@@ -252,12 +252,13 @@ class AktivitasController extends Controller
             }
             $resource = $resource->get();
         } else {
+            $tanggal = date('Y-m-d', strtotime(now()));
             $resource = Area::select(
                 'area.id',
                 'area.nama',
                 'area.kapasitas',
                 DB::raw("TO_CHAR(now(),'YYYY-MM-DD') as tanggal"),
-                DB::raw("COALESCE((SELECT sum(jumlah) FROM area_stok where id_area = area.id and id_material = " . $id_material . " and tanggal = TO_CHAR(now(),'YYYY-MM-DD')),0) as jumlah")
+                DB::raw("COALESCE((SELECT sum(jumlah) FROM area_stok where id_area = area.id and id_material = " . $id_material . " and tanggal = '".$tanggal."') ,0) as jumlah")
             )
             ->leftJoin('area_stok', 'area_stok.id_area', '=', 'area.id')
             ->where(function ($where) use ($search) {
