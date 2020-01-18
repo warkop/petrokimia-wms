a877a6cfba6d9383b7b3c0ed20eccb02bedd1714
warkop619 <navigator619@gmail.com>
2020-01-18 19:16:55 +0700

    shift kerja seeder

--- application/database/seeds/ShiftKerjaSeeder.php
+++ application/database/seeds/ShiftKerjaSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Http\Models\ShiftKerja;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ShiftKerjaSeeder extends Seeder
 {
@@ -18,22 +19,22 @@ class ShiftKerjaSeeder extends Seeder
             'id'            => 1,
             'nama'          => 'Shift 1',
             'mulai'         => '07:00',
-            'akhir'         => '14:30',
+            'akhir'         => '15:00',
             'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
             'created_at'    => date('Y-m-d H:i:s'),
         ]);
         DB::table('shift_kerja')->insert([
             'id'            => 2,
             'nama'          => 'Shift 2',
-            'mulai'         => '14:30',
-            'akhir'         => '22:00',
+            'mulai'         => '15:00',
+            'akhir'         => '23:00',
             'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
             'created_at'    => date('Y-m-d H:i:s'),
         ]);
         DB::table('shift_kerja')->insert([
             'id'            => 3,
             'nama'          => 'Shift 3',
-            'mulai'         => '22:00',
+            'mulai'         => '23:00',
             'akhir'         => '07:00',
             'start_date'    => date('Y-m-d', strtotime("01-01-2019")),
             'created_at'    => date('Y-m-d H:i:s'),
