0aae74e0d8c991bee1650afa8c7ec729431a06b1
warkop619 <navigator619@gmail.com>
2020-01-18 17:38:11 +0700

    g*blk

--- application/app/Http/Controllers/API/AktivitasController.php
+++ application/app/Http/Controllers/API/AktivitasController.php
@@ -332,12 +332,12 @@ class AktivitasController extends Controller
                 ->where('id_material', $id_material)
                 ->where('area_stok.id_area', $id_area);
             if ($aktivitas->produk_rusak != 2) {
-                $detail = $detail->where('area_stok.status = 1');
+                $detail = $detail->where('area_stok.status', 1);
             } else if ($aktivitas->produk_rusak != 1) {
-                $detail = $detail->where('area_stok.status = 2');
+                $detail = $detail->where('area_stok.status', 2);
             } else {
                 if ($aktivitas->produk_stok != null) {
-                    $detail = $detail->where('area_stok.status = 1');
+                    $detail = $detail->where('area_stok.status', 1);
                 }
             }
             if ($aktivitas->fifo != null) {
