3261a462051afa9707fefffcb3b361aef2ec920b
warkop619 <navigator619@gmail.com>
2020-01-23 21:07:53 +0700

    bug tipe

--- application/app/Http/Resources/HistoryMaterialAreaResource.php
+++ application/app/Http/Resources/HistoryMaterialAreaResource.php
@@ -17,7 +17,7 @@ class HistoryMaterialAreaResource extends Resource
         return [
             'id_area'   => $this->areaStok->id_area,
             'nama'      => $this->areaStok->area->nama,
-            'tipe'      => $this->areaStok->area->tipe,
+            'tipe'      => $this->tipe,
             'tanggal'   => $this->areaStok->tanggal,
             'jumlah'    => $this->jumlah,
         ];
