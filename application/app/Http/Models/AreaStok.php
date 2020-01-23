08a60651dc042b51323d3f564a0556b6ae95caee
warkop619 <navigator619@gmail.com>
2020-01-23 15:37:25 +0700

    model area_stok set timestamp automatically

--- application/app/Http/Models/AreaStok.php
+++ application/app/Http/Models/AreaStok.php
@@ -15,14 +15,12 @@ class AreaStok extends Model
 
     protected $hidden = [
         'created_at',
-        'created_by',
         'updated_at',
-        'updated_by',
     ];
 
-    protected $dates = ['start_date', 'end_date', 'created_at', 'updated_at', 'tanggal'];
+    protected $dates = ['created_at', 'updated_at', 'tanggal'];
 
-    public $timestamps  = false;
+    public $timestamps  = true;
 
     public function area()
     {
