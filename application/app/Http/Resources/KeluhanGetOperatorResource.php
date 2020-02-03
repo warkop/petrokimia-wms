f2299df98f19a5bb8d17cbe8892e0390c1126ddc
warkop619 <navigator619@gmail.com>
2020-02-03 20:39:45 +0700

    ke format yang lebih baik

--- application/app/Http/Resources/KeluhanGetOperatorResource.php
+++ application/app/Http/Resources/KeluhanGetOperatorResource.php
@@ -14,23 +14,34 @@ class KeluhanGetOperatorResource extends Resource
      */
     public function toArray($request)
     {
+        $start_date = null;
+        if (!empty($this->tkbm->start_date)) {
+            $start_date = [
+                'date' => date('Y-m-d H:i:s', strtotime($this->tkbm->start_date)),
+                'timezone_type' => 3,
+                'timezone' => 'Asia/Jakarta'
+            ];
+        }
+
+        $end_date = null;
+        if (!empty($this->tkbm->end_date)) {
+            $end_date = [
+                'date' => date('Y-m-d H:i:s', strtotime(
+                    $this->tkbm->end_date
+                )),
+                'timezone_type' => 3,
+                'timezone' => 'Asia/Jakarta'
+            ];
+        }
+
         return [
             'id'            => $this->tkbm->id,
             'job_desk_id'   => $this->tkbm->job_desk_id,
             'nama'          => $this->tkbm->nama,
             'nomor_hp'      => $this->tkbm->nomor_hp,
             'nomor_bpjs'    => $this->tkbm->nomor_bpjs,
-            'start_date'    => [
-                'date' => date('Y-m-d H:i:s', strtotime($this->tkbm->start_date)),
-                'timezone_type' => 3,
-                'timezone' => 'Asia/Jakarta'
-            ],
-            'end_date'      => [
-                'date' => date('Y-m-d H:i:s', strtotime(
-                    $this->tkbm->end_date)),
-                'timezone_type' => 3,
-                'timezone' => 'Asia/Jakarta'
-            ],
+            'start_date'    => $start_date,
+            'end_date'      => $end_date,
             'nik'           => $this->tkbm->nik,
         ];
     }
