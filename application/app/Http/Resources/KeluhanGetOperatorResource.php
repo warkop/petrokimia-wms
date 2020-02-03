10ef02bad438d64b3371008b3c0b8de062b3ba6e
warkop619 <navigator619@gmail.com>
2020-02-03 19:32:30 +0700

    coba

--- application/app/Http/Resources/KeluhanGetOperatorResource.php
+++ application/app/Http/Resources/KeluhanGetOperatorResource.php
@@ -20,17 +20,8 @@ class KeluhanGetOperatorResource extends Resource
             'nama'          => $this->tkbm->nama,
             'nomor_hp'      => $this->tkbm->nomor_hp,
             'nomor_bpjs'    => $this->tkbm->nomor_bpjs,
-            'start_date'    => [
-                'date' => date('Y-m-d H:i:s', strtotime($this->tkbm->start_date)),
-                'timezone_type' => 3,
-                'timezone' => "Asia/Jakarta"
-            ],
-            'end_date'      => [
-                'date' =>  date('Y-m-d H:i:s', strtotime(
-                    $this->tkbm->end_date)),
-                'timezone_type' => 3,
-                'timezone' => "Asia/Jakarta"
-            ],
+            'start_date'    => $this->tkbm->start_date,
+            'end_date'      => $this->tkbm->end_date,
             'nik'           => $this->tkbm->nik,
         ];
     }
