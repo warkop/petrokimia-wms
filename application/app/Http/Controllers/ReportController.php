da03d7b08b7557584daa9791099f3f962eb8b2f7
warkop619 <navigator619@gmail.com>
2020-02-10 09:48:38 +0700

    bug report

--- application/app/Http/Controllers/ReportController.php
+++ application/app/Http/Controllers/ReportController.php
@@ -2493,21 +2493,21 @@ class ReportController extends Controller
             $col++;
             $objSpreadsheet->getActiveSheet()->setCellValueByColumnAndRow($col, $row, (!empty($value->aktivitasHarian->gudangTujuan))?$value->aktivitasHarian->gudangTujuan->nama:'');
             
-            $tempRes =  DB::table('material_trans')
-            ->leftJoin('aktivitas_harian', 'aktivitas_harian.id', '=', 'material_trans.id_aktivitas_harian')
-            ->where('id_material', $value->material->id)
-            ->where('aktivitas_harian.created_at', '<', $value->created_at);
+            // $tempRes =  DB::table('material_trans')
+            // ->leftJoin('aktivitas_harian', 'aktivitas_harian.id', '=', 'material_trans.id_aktivitas_harian')
+            // ->where('id_material', $value->material->id)
+            // ->where('aktivitas_harian.created_at', '<', $value->created_at);
 
-            $penambahan = $tempRes->where('tipe', 2)->sum('jumlah');
-            $pengurangan = $tempRes->where('tipe', 1)->sum('jumlah');
+            // $penambahan = $tempRes->where('tipe', 2)->sum('jumlah');
+            // $pengurangan = $tempRes->where('tipe', 1)->sum('jumlah');
 
-            $jumlahStok = $penambahan+$pengurangan;
+            // $jumlahStok = $penambahan+$pengurangan;
 
-            // if ($value->tipe == 1) {
-            //     $totalStok -= $value->jumlah;
-            // } else {
-            //     $totalStok += $value->jumlah;
-            // }
+            if ($value->tipe == 1) {
+                $totalStok -= $value->jumlah;
+            } else {
+                $totalStok += $value->jumlah;
+            }
 
             $totalStok += $jumlahStok;
 
