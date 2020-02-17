54a1807c7df96450eeb611539cae06f014a87f8f
warkop619 <navigator619@gmail.com>
2020-02-17 08:55:35 +0700

    migrasi aktivitas

---
+++ application/database/migrations/2020_02_17_084102_alter_table_aktivitas10.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableAktivitas10 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('aktivitas', function (Blueprint $table) {
+            $table->string('pengirim_produk_rusak')->nullable();
+            $table->string('cancelable')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('aktivitas', function (Blueprint $table) {
+            $table->dropColumn('pengirim_produk_rusak');
+            $table->dropColumn('cancelable');
+        });
+    }
+}
