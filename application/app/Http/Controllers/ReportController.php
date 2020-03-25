baa97f9a4a517070f33d84a048303b5d1d05fd6e
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-03-25 14:33:33 +0700

    harus bisa

--- application/app/Http/Controllers/ReportController.php
+++ application/app/Http/Controllers/ReportController.php
@@ -4218,15 +4218,15 @@ class ReportController extends Controller
                         ->leftJoin('aktivitas_harian', function ($join) use ($tanggal) {
                             $join->on('aktivitas_harian.id', '=', 'material_trans.id_aktivitas_harian')
                                 ->where('draft', 0)
-                                ->where(DB::raw("TO_CHAR(aktivitas_harian.updated_at, 'yyyy-mm-dd HH24-MI-SS')"), '<', date('Y-m-d H:i:s', strtotime($tanggal . ' 07:00:00')));
+                                ->whereBetween(DB::raw("TO_CHAR(aktivitas_harian.updated_at, 'yyyy-mm-dd HH24-MI-SS')"), [date('Y-m-d H:i:s', strtotime($tanggal . ' 07:00:00')), date('Y-m-d H:i:s', strtotime($tanggal . ' 23:00:00 -1 day'))]);
                         })
                         ->leftJoin('material_adjustment', function ($join) use ($tanggal) {
                             $join->on('material_adjustment.id', '=', 'material_trans.id_adjustment')
                                 // ->where('material_adjustment.tanggal', $tanggal);
                                 ->where('material_adjustment.shift', '=', 3)
-                                ->orWhere(function($query) use ($tanggal){
+                                ->where(function($query) use ($tanggal){
                                     $query->where('material_adjustment.tanggal', '<', $tanggal);
-                                    $query->where('material_adjustment.tanggal', '=', $tanggal);
+                                    $query->orWhere('material_adjustment.tanggal', '=', $tanggal);
                                 });
                         })
                         ->where(
