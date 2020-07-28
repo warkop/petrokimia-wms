8666d208f89da39b164ab06cf7cf73c069ea4c7a
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-07-28 14:42:02 +0700

    simpan centangan posto

--- application/app/Http/Controllers/AktivitasController.php
+++ application/app/Http/Controllers/AktivitasController.php
@@ -98,6 +98,7 @@ class AktivitasController extends Controller
         $aktivitas->tanda_tangan               = $req->input('tanda_tangan');
         $aktivitas->butuh_approval             = $req->input('butuh_approval');
         $aktivitas->so                         = $req->input('so');
+        $aktivitas->posto                      = $req->input('posto');
         $aktivitas->status_aktivitas           = $req->input('status_aktivitas');
         // $aktivitas->tanpa_tanggal              = $req->input('tanpa_tanggal');
         $aktivitas->penyusutan                 = $req->input('penyusutan');
